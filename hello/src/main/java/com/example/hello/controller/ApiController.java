bdcacfceabeabdb1955946a70eaa1d86a39e0008
MinseoKangQ <kms02171@naver.com>
2023-01-15 17:04:58 +0900

    단순히 문자열을 리턴하는 API 작성(GET 방식)

---
+++ hello/src/main/java/com/example/hello/controller/ApiController.java
@@ -0,0 +1,17 @@
+package com.example.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //  해당 Class는 REST API 처리하는 Controller로 등록
+@RequestMapping("/api") // URI를 지정해주는 Annotation
+public class ApiController {
+
+    // GET방식으로 요청이 들어오면, 문자열 리턴
+    @GetMapping("/hello") // http://localhost:9090/api/hello 로 맵핑
+    public String hello() {
+        return "hello spring boot!";
+    }
+    
+}
