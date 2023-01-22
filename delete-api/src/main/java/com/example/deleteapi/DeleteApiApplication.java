23ea6d0dbc90896e08f69639edae9362d754528d
MinseoKangQ <kms02171@naver.com>
2023-01-22 21:21:29 +0900

    DELETE API 작성

---
+++ delete-api/src/main/java/com/example/deleteapi/DeleteApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.deleteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeleteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeleteApiApplication.class, args);
+	}
+
+}
