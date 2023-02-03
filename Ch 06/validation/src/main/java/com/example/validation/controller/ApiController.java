3fc1b1f2f98c875be015d64292bd81c15d58dcc4
MinseoKangQ <kms02171@naver.com>
2023-02-03 13:38:44 +0900

    validation added

---
+++ Ch 06/validation/src/main/java/com/example/validation/controller/ApiController.java
@@ -0,0 +1,48 @@
+package com.example.validation.controller;
+
+import com.example.validation.dto.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    @PostMapping("/user") // 리소스 생성, 추가
+    public ResponseEntity user(@Valid @RequestBody User user, BindingResult bindingResult) {
+
+        if(bindingResult.hasErrors()) { // Error를 가지고 있다면
+            // 무슨 Error인지 보기
+            StringBuilder sb = new StringBuilder();
+            bindingResult.getAllErrors().forEach(objectError -> {
+                FieldError field = (FieldError) objectError;
+                String message = objectError.getDefaultMessage();
+
+                System.out.println("field : " + field.getField());
+                System.out.println(message);
+
+                sb.append("field : " + field.getField());
+                sb.append("message : " + message);
+            });
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(sb.toString());
+        }
+        System.out.println(user);
+
+        // 옛날 코드
+//        if(user.getAge() >= 90) {
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(user);
+//        }
+
+        return ResponseEntity.ok(user);
+    }
+
+}
