0cee991f80d39dd5fd6a54485af4a2015133f5aa
MinseoKangQ <kms02171@naver.com>
2023-02-04 17:01:37 +0900

    excpetion added

---
+++ Ch 06/exception/src/main/java/com/example/exception/advice/GlobalControllerAdvice.java
@@ -0,0 +1,27 @@
+package com.example.exception.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalControllerAdvice {
+
+    @ExceptionHandler(value = Exception.class) // 모든 예외를 잡아서 예외 처리
+    public ResponseEntity exception(Exception e) { // 매개변수로 예외를 받을 수 있음
+        System.out.println(e.getClass().getName());
+        System.out.println("-----");
+        System.out.println(e.getLocalizedMessage());
+        System.out.println("-----");
+        // response로 내려주는 부분
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("");
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class) // 특정 메소드의 예외 잡아서 예외 처리
+    public ResponseEntity methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        // Response의 Body에 메세지 넘겨줌
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+}
---
+++ Ch 06/exception/src/main/java/com/example/exception/controller/ApiController.java
@@ -0,0 +1,39 @@
+package com.example.exception.controller;
+
+import com.example.exception.dto.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/user")
+public class ApiController {
+
+    @GetMapping("")
+    public User get(@RequestParam(required = false) String name, @RequestParam(required = false) Integer age) {
+        User user = new User();
+        user.setName(name);
+        user.setAge(age);
+
+        int a = 10 + age;
+
+        return user;
+    }
+
+    @PostMapping("")
+    public User post(@Valid @RequestBody User user) {
+        System.out.println(user);
+        return user;
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class) // 특정 메소드의 예외 잡아서 예외 처리
+    public ResponseEntity methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        // Response의 Body에 메세지 넘겨줌
+        System.out.println("api controller에서 동작");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+}
