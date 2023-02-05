2bf3ca4040491dc5c0a27e0ea57ec1f1a58e77af
MinseoKangQ <kms02171@naver.com>
2023-02-05 17:50:10 +0900

    exception-and-validation added

---
+++ Ch 06/exception-and-validation/src/main/java/com/example/exceptionandvalidation/ExceptionAndValidationApplication.java
@@ -0,0 +1,13 @@
+package com.example.exceptionandvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionAndValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionAndValidationApplication.class, args);
+	}
+
+}
---
+++ Ch 06/exception-and-validation/src/test/java/com/example/exceptionandvalidation/ExceptionAndValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exceptionandvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionAndValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
