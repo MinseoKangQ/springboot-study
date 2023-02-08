004bcf990585caabb0578e3b086e7f67e50b50b3
MinseoKangQ <kms02171@naver.com>
2023-02-08 15:44:43 +0900

    server added

---
+++ Ch 07/server/src/main/java/com/example/server/controller/ServerApiController.java
@@ -0,0 +1,67 @@
+package com.example.server.controller;
+
+import com.example.server.dto.Req;
+import com.example.server.dto.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/server")
+public class ServerApiController {
+
+    @GetMapping("/hello")
+    public User hello(@RequestParam String name, @RequestParam int age) {
+        User user = new User();
+        user.setName(name);
+        user.setAge(age);
+        return user;
+    }
+//  post
+//    @PostMapping("/user/{userId}/name/{userName}")
+//    public User post(@RequestBody User user,
+//                     @PathVariable int userId,
+//                     @PathVariable String userName) {
+//        log.info("userId : {}, userName : {}", userId, userName);
+//        log.info("client req : {}", user);
+//        return user;
+//    }
+
+//exchange 확인
+//    @PostMapping("/user/{userId}/name/{userName}")
+//    public User post(@RequestBody User user,
+//                     @PathVariable int userId,
+//                     @PathVariable String userName,
+//                     @RequestHeader("x-authorization") String authorization,
+//                     @RequestHeader("custom-header") String customHeader) {
+//        log.info("userId : {}, userName : {}", userId, userName);
+//        log.info("authorization : {}, custom : {}", authorization, customHeader);
+//        log.info("client req : {}", user);
+//        return user;
+//    }
+//}
+
+//genericExchange 확인
+    @PostMapping("/user/{userId}/name/{userName}")
+    public Req<User> post(
+//                          HttpEntity<String> entity,
+                          @RequestBody Req<User> user,
+                          @PathVariable int userId,
+                          @PathVariable String userName,
+                          @RequestHeader("x-authorization") String authorization,
+                          @RequestHeader("custom-header") String customHeader)
+    {
+//        log.info("req : {}", entity.getBody());
+        log.info("userId : {}, userName : {}", userId, userName);
+        log.info("authorization : {}, custom : {}", authorization, customHeader);
+        log.info("client req : {}", user);
+
+        Req<User> response = new Req<>();
+        response.setHeader(
+                new Req.Header()
+        );
+        response.setResBody(user.getResBody());
+        return response;
+    }
+}
