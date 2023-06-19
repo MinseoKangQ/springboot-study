023dc9ace345513ea18b7d7afb568c1384ffca9e
MinseoKangQ <kms02171@naver.com>
2023-06-19 16:42:52 +0900

    controller-thymeleaf 프로젝트 - HTML Template 사용, Model 객체, ModelAndView 객체 사용

---
+++ likelion/controller-thymeleaf/src/main/java/dev/MinseoKangQ/controllerthymeleaf/ControllerThymeleafApplication.java
@@ -0,0 +1,13 @@
+package dev.MinseoKangQ.controllerthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControllerThymeleafApplication.class, args);
+	}
+
+}
