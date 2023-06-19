52b4bbfb9a0e99e206caa44c11d2b0115be8b97b
MinseoKangQ <kms02171@naver.com>
2023-06-19 16:40:12 +0900

    controller-demo 프로젝트 - Postman 사용 실습, @ResponseStatus, 이미지 전송

--- likelion/controller-demo/src/main/java/dev/MinseoKangQ/controllerdemo/SampleRestController.java
+++ likelion/controller-demo/src/main/java/dev/MinseoKangQ/controllerdemo/SampleRestController.java
@@ -2,10 +2,10 @@ package dev.MinseoKangQ.controllerdemo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -18,7 +18,7 @@ public class SampleRestController {
     private static final Logger logger = LoggerFactory.getLogger(SampleRestController.class);
 
     @GetMapping("/sample-payload")
-    public SamplePayload samplePayload() {
+    public SamplePayload samplePayloadGet() {
         return new SamplePayload("aquashdw", 10, "Developer");
     }
 
@@ -30,4 +30,29 @@ public class SampleRestController {
         InputStream inputStream = getClass().getResourceAsStream("/static/img.png");
         return inputStream.readAllBytes();
     }
+
+    @PostMapping("/sample-payload")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void samplePayloadPost(@RequestBody SamplePayload samplePayload) {
+        logger.info(samplePayload.toString());
+    }
+
+    @PostMapping(
+            value = "/sample-multipart",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE
+    )
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void sampleMultipartPost(
+            @RequestParam("name") String name,
+            @RequestParam("age") Integer age,
+            @RequestParam("occupation") String occupation,
+            @RequestParam("file") MultipartFile multipartFile
+    ) {
+        logger.info("name : {}", name);
+        logger.info("age : {}", age);
+        logger.info("occupation : {}", occupation);
+        logger.info("file original name : {}", multipartFile.getOriginalFilename());
+    }
+
+
 }
