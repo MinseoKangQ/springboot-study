cf538da9c16d9174e4402f8b0403331c4ab46035
MinseoKangQ <kms02171@naver.com>
2023-10-03 19:15:37 +0900

    2차 미션 해설 영상2  - 미디어 구현

---
+++ likelion/community/src/main/java/com/likelion/community/service/MediaService.java
@@ -0,0 +1,12 @@
+package com.likelion.community.service;
+
+import com.likelion.community.model.MediaDescriptorDto;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Collection;
+
+public interface MediaService {
+    MediaDescriptorDto saveFile(MultipartFile file);
+    Collection<MediaDescriptorDto> saveFileBulk(MultipartFile[] files);
+    byte[] getFileAsBytes(String resourcePath);
+}
