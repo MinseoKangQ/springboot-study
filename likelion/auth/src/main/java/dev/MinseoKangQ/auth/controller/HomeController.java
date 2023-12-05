7df9462e00fd1e0c166a24a7b548f20f84ae5b45
MinseoKangQ <kms02171@naver.com>
2023-12-05 16:20:15 +0900

    Login 커스텀 실습 완료

--- likelion/auth/src/main/java/dev/MinseoKangQ/auth/controller/HomeController.java
+++ likelion/auth/src/main/java/dev/MinseoKangQ/auth/controller/HomeController.java
@@ -1,7 +1,9 @@
 package dev.MinseoKangQ.auth.controller;
 
+import dev.MinseoKangQ.auth.infra.AuthenticationFacade;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +13,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+    private final AuthenticationFacade authFacade;
 
+    public HomeController(@Autowired AuthenticationFacade authFacade) {
+        this.authFacade = authFacade;
+    }
     @GetMapping
     public String home() {
+        try {
+            logger.info("connected user: {}", authFacade.getUserName()); // 로그인 유저 확인
+        } catch (NullPointerException e) {
+            logger.info("no user logged in");
+        }
         return "index";
     }
 
---
+++ likelion/auth/src/main/java/dev/MinseoKangQ/auth/entity/UserEntity.java
@@ -0,0 +1,83 @@
+package dev.MinseoKangQ.auth.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+//@Getter
+//@Setter
+@Table(name = "USER_ENTITY")
+public class UserEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column
+    private String password;
+
+    @Column
+    private String nickname;
+
+    @Column
+    private String role;
+
+    public UserEntity() {
+    }
+
+    public UserEntity(Long id, String username, String password, String nickname, String role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.nickname = nickname;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
