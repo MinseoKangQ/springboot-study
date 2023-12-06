3a945094df9d2d3c950ecde16bde168fae2d3667
MinseoKangQ <kms02171@naver.com>
2023-12-06 16:06:27 +0900

    Message Broker 활용 기본 실습 완료

---
+++ likelion/producer/src/main/java/com/MinseoKangQ/producer/controller/ProducerController.java
@@ -0,0 +1,21 @@
+package com.MinseoKangQ.producer.controller;
+
+import com.MinseoKangQ.producer.service.ProducerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProducerController {
+
+    private final ProducerService producerService;
+
+    public ProducerController(@Autowired ProducerService producerService) {
+        this.producerService = producerService;
+    }
+
+    @GetMapping("/")
+    public void sendMessage() {
+        producerService.send();
+    }
+}
