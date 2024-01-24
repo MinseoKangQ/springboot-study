747cbe3c33e597a45b14473fe8e59f48b35626a5
MinseoKangQ <kms02171@naver.com>
2024-01-24 15:04:48 +0900

    컴포넌트 스캔 (+ 테스트 시 로그 안뜸 문제 해결을 위해 logback.xml 추가

---
+++ likelion-study-inflearn/스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,20 @@
+package hello.core;
+
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration // 설정 정보
+@ComponentScan( // 자동으로 스프링 빈 끌어올림 - @Component 붙은걸 찾아서 빈으로 등록
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) // 기존 만들어 놓은 AppConfig 는 제외
+)
+public class AutoAppConfig {
+
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
