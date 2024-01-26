9f9fc9c572fae3689894a28480b047fd61c0ab7c
MinseoKangQ <kms02171@naver.com>
2024-01-26 14:08:21 +0900

    의존관계 자동 주입

---
+++ likelion-study-inflearn/스프링 핵심 원리 - 기본편/core/src/test/java/hello/core/autowired/AutoWiredTest.java
@@ -0,0 +1,39 @@
+package hello.core.autowired;
+
+import hello.core.member.Member;
+import jakarta.annotation.Nullable;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Optional;
+
+public class AutoWiredTest {
+
+    @Test
+    void AutowiredOption() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class); // Test 를 스프링 빈으로 등록
+    }
+
+    static class TestBean {
+
+        // 의존 관계가 없으면 호출 자체가 안됨
+        @Autowired(required = false)
+        public void setNoBean1(Member noBean1) {
+            System.out.println("noBean1 = " + noBean1);
+        }
+
+        // 호출은 가능하지만 null 로 들어옴
+        @Autowired
+        public void setNoBean2(@Nullable Member noBean2) {
+            System.out.println("noBean2 = " + noBean2);
+        }
+
+        // Optional Java8 공부
+        @Autowired
+        public void setNoBean3(Optional<Member> noBean3) {
+            System.out.println("noBean3 = " + noBean3);
+        }
+    }
+}
